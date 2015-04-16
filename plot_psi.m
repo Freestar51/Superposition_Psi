13df1f87043b90b33eeffb974dcd014f083d51cd
Freestar51 <freestar51@unist.ac.kr>
2015-04-16 16:51:44 +0900

    plot save

--- plot_psi.m
+++ plot_psi.m
@@ -1,8 +1,8 @@
 % Plot result of psi function in [0,1]
 
-D = [0:0.0000001:1];
+D = [0:0.00001:1];
 D = D(1:end-1);
-n = 2;
+n = 4;
 gamma = 10;
 k = 5;
 
@@ -11,4 +11,8 @@ parfor idx = 1:length(D)
     p(idx) = psi_func(D(idx), n, gamma, k);
 end
 
+save(sprintf('./psi_result_n%d_gamma_%d.mat',n,gamma),'p');
+figure;
+set(gcf, 'position', [0 0 1080 1080]);
 plot(D, p);
+saveas(gcf, sprintf('./psi_result_n%d_gamma_%d.jpg',n,gamma),'jpg');
